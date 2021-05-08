1cd65104d2cf07d78994ddd2705b1083ce42d5dc
youngpierre24 <neilpl24@live.unc.edu>
2021-05-08 14:46:14 -0400

    css

--- public/script.js
+++ public/script.js
@@ -42,7 +42,7 @@ async function displaySchedule() {
             const homeTeam = game.gameData.teams.home.name;
             const awayScore = game.liveData.linescore.teams.away.goals;
             const homeScore = game.liveData.linescore.teams.home.goals;
-            const displayDiv = $(`<div class="card text-white bg-secondary mb-3" style="width: 18rem;display:inline-block;"></div>`);
+            const displayDiv = $(`<div class="card text-white bg-secondary mb-3" style="width: 18rem;display:inline-block;float:left;margin: 10px 10px 10px 10px;"></div>`);
             displayDiv.append($(`<div class="card-header">
             ︎ </div>`));
             displayDiv.append($(`<img src="/logos/${homeTeam}1.png" class="card-img-top" width="30px" alt="NHL">`));
