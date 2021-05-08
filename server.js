5d46c1700a955f06e6a594796ce4e3c61f5899b6
youngpierre24 <neilpl24@live.unc.edu>
2021-05-07 23:57:25 -0400

    cors issue

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.listen(80, () => {
     }
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
     app.post('/pick', async (request, response) => {
         const updateProfile = await profileModel.findOneAndUpdate({
