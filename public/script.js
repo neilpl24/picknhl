389dba351dd816e2c24303b9c0e84a470cd6f1f7
youngpierre24 <neilpl24@live.unc.edu>
2021-05-08 14:53:00 -0400

    fixed card height

--- public/script.js
+++ public/script.js
@@ -42,7 +42,7 @@ async function displaySchedule() {
             const homeTeam = game.gameData.teams.home.name;
             const awayScore = game.liveData.linescore.teams.away.goals;
             const homeScore = game.liveData.linescore.teams.home.goals;
-            const displayDiv = $(`<div class="card text-white bg-secondary mb-3" style="width: 18rem;display:inline-block;float:left;"></div>`);
+            const displayDiv = $(`<div class="card text-white bg-secondary mb-3" style="width:18rem;min-height:139px;display:inline-block;float:left;"></div>`);
             displayDiv.append($(`<div class="card-header">
             ︎ </div>`));
             displayDiv.append($(`<img src="/logos/${homeTeam}1.png" class="card-img-top" width="30px" alt="NHL">`));
